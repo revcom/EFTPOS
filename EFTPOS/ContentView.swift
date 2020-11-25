4c0eeb8a2ec8647aa31d225a9c98abdbde186eb5
Robert Crago <robert@revComputing.com>
2020-11-26 05:38:35 +1000

    Initial Commit

---
+++ EFTPOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EFTPOS
+//
+//  Created by Robert Crago on 26/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EFTPOS/EFTPOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  EFTPOSApp.swift
+//  EFTPOS
+//
+//  Created by Robert Crago on 26/11/20.
+//
+
+import SwiftUI
+
+@main
+struct EFTPOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
