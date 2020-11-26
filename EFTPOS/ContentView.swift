3cbc14afada991eb68f2a39fff2c034ecc8a304d
Robert Crago <robert@revComputing.com>
2020-11-27 05:37:35 +1000

    Trying fresh approach

--- EFTPOS/ContentView.swift
+++ EFTPOS/ContentView.swift
@@ -8,23 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var scanner = NFCScan()
+
     @State var amount = 0.00
-
+    
     var body: some View {
-        VStack( alignment: .leading, spacing: 10, content: {
-            HStack {
-                Text("Pay BWAC:")
+        if scanner.nfcAvailable() {
+            VStack( alignment: .leading, spacing: 10, content: {
+                HStack {
+                    Text("Pay BWAC:")
 
-                TextField("Amount", value: $amount, formatter: NumberFormatter.currency,
-                          onEditingChanged: {_ in },
-                          onCommit: {
-                            NFCScan().scan()
-                          })
-                    .keyboardType(.numbersAndPunctuation)
-                    .font(.title)
-
-            }
-        })
+                    TextField("Amount", value: $amount, formatter: NumberFormatter.currency,
+                              onEditingChanged: {_ in },
+                              onCommit: {
+    //                            NFCScan().scan()
+                              })
+                        .keyboardType(.numbersAndPunctuation)
+                        .font(.title)
+                        .alert(isPresented: $scanner.showAlert) {
+                            Alert(title: Text("Sorry"), message: Text(scanner.alertMessage), dismissButton: .default(Text("OK")))
+                        }
+                }
+            })
+        }
+        Text("❌ Scanning not supported on this iPhone")
     }
 }
 
--- EFTPOS/NFCScan.swift
+++ EFTPOS/NFCScan.swift
@@ -5,83 +5,20 @@
 //  Created by Robert Crago on 26/11/20.
 //
 
+import UIKit
 import CoreNFC
 
-protocol NFCReaderDelegate {
-    func didReceive(payload: String)
-}
+class NFCScan: ObservableObject {
+    @Published var alertMessage = "Alert me!"
+    @Published var showAlert = false
 
-class NFCScan: NFCReaderDelegate {
-    func didReceive(payload: String) {
-        print ("Received: \(payload)")
-    }
-    
-    var nfcReader: NFCReader? = nil
-    func scan() {
-        if nfcReader == nil {
-            nfcReader = NFCReader()
-            nfcReader?.delegate = self
+    func nfcAvailable() -> Bool {
+        guard NFCNDEFReaderSession.readingAvailable else {
+            alertMessage = "This device doesn't support tag scanning"
+            showAlert = true
+            return false
         }
-        nfcReader?.begin()
-    }
-}
-
-
-class NFCReader: NSObject {
-    private var session: NFCNDEFReaderSession?
-    var delegate: NFCReaderDelegate?
-    
-    func begin() {
-        session = NFCNDEFReaderSession(delegate: self,
-                                       queue: DispatchQueue.main,
-                                       invalidateAfterFirstRead: false)
-        session?.begin()
-    }
-}
-
-extension NFCReader: NFCNDEFReaderSessionDelegate {
-    
-    func readerSession(
-      _ session: NFCNDEFReaderSession,
-      didDetect tags: [NFCNDEFTag]
-    ) {
-      guard
-        let tag = tags.first,
-        tags.count == 1
-        else {
-          session.alertMessage = """
-            There are too many tags present. Remove all and then try again.
-            """
-          DispatchQueue.global().asyncAfter(deadline: .now() + .milliseconds(500)) {
-            session.restartPolling()
-          }
-          return
-      }
-        tag.queryNDEFStatus { (status, _, error) in
-            switch (status) {
-            case .notSupported: print ("Unsupported tag")
-            case .readOnly: print ("ReadOnly tag")
-            case  .readWrite: print ("ReadWrite tag");
-                session.invalidate()
-            default: print ("Default tag")
-            }
-        }
-    }
-
-    func readerSession(_ session: NFCNDEFReaderSession,
-                       didDetectNDEFs messages: [NFCNDEFMessage]) {
-        messages.forEach { message in
-            message.records.forEach { record in
-                if let string = String(data: record.payload, encoding: .ascii) {
-                    print ("Type: \(record.type) Payload: \(record.payload)")
-                    delegate?.didReceive(payload: string)
-                }
-            }
-        }
-    }
-    func readerSession(_ session: NFCNDEFReaderSession,
-                       didInvalidateWithError error: Error) {
-        print(error.localizedDescription)
+        return false
     }
 }
 
